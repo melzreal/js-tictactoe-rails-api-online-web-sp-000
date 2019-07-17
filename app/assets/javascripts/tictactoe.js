8d32e92a029ca910560b6d4054486983c98ba5e1
melzreal <melissa.real@hotmail.com>
2019-07-17 18:24:32 +0100

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -17,49 +17,59 @@ function player(){
 
 
 function updateState(td){
-	// var currentPlayer = player();
-	// $(td).text(currentPlayer);
+	var currentPlayer = player();
+	$(td).text(currentPlayer);
 
-	$('table tr td').on("click", function() {
-		currentPlayer = player();
-		$(this).text(currentPlayer);
-		turn++;
-	});
+	// $('table tr td').on("click", function() {
+	// 	currentPlayer = player();
+	// 	$(this).text(currentPlayer);
+	// 	turn++;
+	// });
 	
 }
 
 function setMessage(string){
-
 	$("#message").text(string);
-
 }
 
 function checkWinner(){
 
-WIN_COMBINATIONS.forEach(function(check){
+	var squareAgain = window.document.querySelectorAll('td')
+	var winner = "none";
+	var board = {
+	length: 0,
+    addElem: function addElem(el) {
+        [].push.call(this, el);
+    	}
+	};
 
-		
-});
+ for (var i = 0; i < squareAgain.length; i++) {
+  board.addElem(squareAgain[i].innerHTML);
+}
 
- // setMessage('Player X Won!');
+	for (let i = 0; i < WIN_COMBINATIONS.length; i++) {
+    const [a, b, c] = WIN_COMBINATIONS[i];
 
- // setMessage('Player Y Won!');
+    	if (board[a] && board[a] === board[b] && board[a] === board[c] && board[a] !=="") {
+     	 winner = board[a];
 
+		  if (winner === "O"){
+		 	setMessage('Player O Won!');
+   		 } else if (winner === "X"){ 
+		  setMessage('Player X Won!');
+		 } 
+		 return true;
+	// 	console.log(winner);
+ //        return true;
+	//  } else {
+ //  	setMessage('Its a draw!'); 	
+ //  	return false;
 
+	 	} 
+   } return false;
 
 }
 
-function position_taken(index){
-var square = window.document.querySelectorAll('td');
-
-   if (square[index]=== "X" || square[index] === "O"){
-   	return true; 
-   } else {
-   	return false;
-
-   	} 
-}
- 
 
 function doTurn(){
 	turn++;
